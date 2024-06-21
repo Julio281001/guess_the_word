2b9d8999edb3980e5ea6a7e805d27c1c9d904410
Julio Cesar Estrada Flores <julio@DESKTOP-77J3O3M.localdomain>
2024-06-20 21:42:53 -0600

    Mostrar una alerta para hacer sabe al usuario si adivino o no la palabra

--- js/main.js
+++ js/main.js
@@ -3,55 +3,9 @@
 const inputsContainer = document.querySelector("#inputsContainer");
 const wordParagrahp = document.querySelector("#scrambledWord");
 
-// const addRequiredInputs = function (word) {
-//     for (let i = 0; i < word.length; i++) {
-//         const input = document.createElement("input");
-//         input.type = "text";
-//         input.maxLength = 1;
-//         input.style.padding = "5px";
-//         input.style.margin = "5px";
-//         input.style.textAlign = "center";
-//         input.style.width = "30px";
-//         input.style.fontSize = "14px";
-//         inputsContainer.appendChild(input);
-//         if (i == 0) {
-//             input.focus();
-//         }
-//         else {
-//             input.disabled = true;
-//         }
-//         input.addEventListener("input", function () {
-//             if (this.value) {
-//                 if (word.includes(this.value)) {
-//                     if (input.nextElementSibling) {
-//                         input.nextElementSibling.disabled = false;
-//                         input.nextElementSibling.focus();
-//                     }
-//                     input.disabled = true;
-//                 } else {
-//                     this.value = "";
-//                 }
-//             }
-//         });
-//     }
-// }
-
-// const displayWord = function (word) {
-//     wordParagrahp.textContent = word.split('').sort(() => Math.random() - 0.5).join('');;
-// }
-
-// const getWord = function () {
-//     fetch("https://random-word-form.herokuapp.com/random/noun/")
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data);
-//             displayWord(data[0]);
-//             addRequiredInputs(data[0]);
-//         });
-// }
-
 const addInputs = function (word) {
     const remainingLetters = word.split('');
+    let userGuess = "";
     for (let i = 0; i < word.length; i++) {
         const input = document.createElement("input");
         input.type = "text";
@@ -71,13 +25,20 @@ const addInputs = function (word) {
         input.addEventListener("input", function () {
             if (this.value) {
                 if (remainingLetters.includes(this.value)) {
+                    userGuess += this.value;
                     const index = remainingLetters.indexOf(this.value);
                     remainingLetters.splice(index, 1);
+                    input.disabled = true;
                     if (input.nextElementSibling) {
                         input.nextElementSibling.disabled = false;
                         input.nextElementSibling.focus();
+                    } else {
+                        if (userGuess === word) {
+                            alert("You win!");
+                        } else {
+                            alert("You lose!");
+                        }
                     }
-                    input.disabled = true;
                 } else {
                     this.value = "";
                 }
@@ -108,4 +69,6 @@ const main = async function () {
     console.log(word);
     displayWord(word);
     addInputs(word);
-}();
+};
+
+main();
